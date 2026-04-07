48f6f1e55c360bec92dd2d94bd808bae143d026d
Himanshu Gullaiya <himanshugullaiya@gmail.com>
2026-04-07 23:31:27 +0530

    Add all 6 gold layer materialized views

--- SQL/D2C_06_executive_kpis.sql
+++ SQL/D2C_06_executive_kpis.sql
@@ -1,16 +1,32 @@
-SELECT * FROM silver.orders LIMIT 5;
-SELECT * FROM silver.order_items LIMIT 5;
-SELECT * FROM silver.products LIMIT 5;
-SELECT * FROM silver.orders LIMIT 5;
+------------------Execute KPIs--------
 
+DROP MATERIALIZED VIEW IF EXISTS gold.executive_kpis;
 
-SELECT 
-	sum(o.net_amount) AS total_revenue,
-	count(DISTINCT o.order_id) AS total_orders,
-	count(DISTINCT o.customer_id) AS total_customers,
-	sum(o.net_amount) / count(DISTINCT o.order_id) AS AOV,
-	sum(o.net_amount) - sum(oi.qty)
+CREATE MATERIALIZED VIEW gold.executive_kpis AS (
 
-
+WITH total AS (
+	SELECT 
+		date_trunc('month', o.order_date)::date AS for_sorting,
+		to_char(o.order_date, 'FMMonth YYYY') AS year_month,
+		sum(o.net_amount) AS total_revenue,
+		count(DISTINCT o.order_id) AS total_orders,
+		count(DISTINCT o.customer_id) AS total_customers,
+		sum(o.net_amount) / count(DISTINCT o.order_id) AS AOV,
+		(sum(oi.total_sales_curr_order) - sum(oi.qty * p.cost_price)) / NULLIF(sum(oi.total_sales_curr_order),0) AS profit_margin_pct_decimal,
+		count(DISTINCT ar.return_id)*1.0 / NULLIF(count(DISTINCT o.order_id),0) AS return_rate_pct_decimal,
+		COUNT(DISTINCT co.order_id) * 1.0 / NULLIF(COUNT(DISTINCT o.order_id), 0) AS cancelled_order_rate_decimal
 	
-SELECT * FROM silver.order_items oi LEFT JOIN orders o ON oi.order_id = o.order_id;
+		FROM silver.order_items oi 
+		LEFT JOIN silver.orders o ON o.order_id = oi.order_id
+		LEFT JOIN silver.products p ON p.product_id = oi.product_id
+		LEFT JOIN silver.all_returns ar ON ar.order_id = o.order_id
+		LEFT JOIN (SELECT order_id FROM silver.orders WHERE order_status = 'Cancelled') co ON co.order_id = o.order_id
+	GROUP BY 1,2
+)
+
+SELECT * FROM total
+);
+
+REFRESH MATERIALIZED VIEW gold.executive_kpis;
+
+SELECT * FROM gold.executive_kpis LIMIT 5;
